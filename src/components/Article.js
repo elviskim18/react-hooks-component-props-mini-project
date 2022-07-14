0fda467d663fd4a1c5e4f0b043b456fbb325a76f
Kimani <elvis.kimani@student.moringaschool.com>
2022-07-14 15:53:02 +0300

    commit tree 4667a45bf49c6f918a8ab9e3b236ed27e6ab6e94

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,24 @@
 import React from "react";
 
+function displaytime(minutes){
+    const emoji = minutes < 30? "☕️" : "🍱"
+    const number = minutes < 30 ?  5:10 
+    let emojis = ""
+    for (let i = 0 ; i <=minutes; i+= number){
+        emojis += emoji
+    }
+    const text = emojis + " "+ minutes + " "+ "min to read"
+    return text
+}
+
 function Article ({title, date ="January 1, 1970", preview , minutes}){
+    
+
     return (
+        
         <article>
             <h3>{title}</h3>
-            <small>{date}.</small>
+            <small>{date}. {displaytime(minutes)}</small>
             <p>{preview}</p>
         </article>
     )
